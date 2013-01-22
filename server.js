2991e530b770e060164824efcd2ffd7a210660da
Jose F. Romaniello <jfromaniello@gmail.com>
2013-01-22 16:52:59 -0300

    minor

--- server.js
+++ server.js
@@ -11,19 +11,20 @@ app.configure(function(){
   this.use(express.static(__dirname  + "/public"));
 });
 
-var url = require('url');
-var redisUrl = url.parse(process.env.REDISTOGO_URL);
-//setup share
-share.attach(app, {
-  db: {
-    type:     'redis',
-    hostname: redisUrl.hostname,
-    port:     redisUrl.port,
-    auth:     redisUrl.auth,
-    prefix:   process.env.REDIS_PREFIX || 'super'
-  }
-  // db: {type: 'none'} or redis
-});
+var sharedb = { type: 'redis' };
+
+if (process.env.REDISTOGO_URL) { 
+  var url = require('url');
+  var redisUrl = url.parse(process.env.REDISTOGO_URL);
+
+  sharedb.type     = 'redis';
+  sharedb.hostname = redisUrl.hostname;
+  sharedb.port     = redisUrl.port;
+  sharedb.auth     = redisUrl.auth;
+  sharedb.prefix   = process.env.REDIS_PREFIX || 'super';
+}
+
+share.attach(app, { db: sharedb });
 
 http.createServer(app).listen(port, function(){
   console.log('listening in http://localhost:' + port);
